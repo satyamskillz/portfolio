d1dd8a76c8f71e56e68c5672e6037b7794502970
Satyamskillz <satyamskillz@gmail.com>
2020-03-17 00:18:58 +0530

    added readme file

--- jobs/models.py
+++ jobs/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class project(models.Model):
+class jobs(models.Model):
     image=models.ImageField(upload_to="images/")
-    summary=models.CharField(max_length=200)
+    company_name=models.CharField(max_length=100)
+    post=models.CharField(max_length=100)
+    start_date=models.DateField()
+    end_date=models.DateField()
+    body=models.TextField()
+    link=models.CharField(max_length=100)
--- projects/models.py
+++ projects/models.py
@@ -3,5 +3,8 @@ from django.db import models
 # Create your models here.
 
 class project(models.Model):
-    image=models.ImageField(upload_to="images/")
-    summary=models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    sub_title=models.CharField(max_length=200)
+    date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to="images/")
